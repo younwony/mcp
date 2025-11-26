8350874d04e06fc73dc25b0e827f5e273af609fd
younwony <wony9324@naver.com>
2025-11-26 10:04:36 +0900

    feat: mcp 서버 생성 완료.

--- src/main/java/com/example/mcp/McpApplication.java
+++ src/main/java/com/example/mcp/McpApplication.java
@@ -1,13 +1,22 @@
 package com.example.mcp;
 
+import com.example.mcp.tool.WeatherService;
+import org.springframework.ai.tool.ToolCallbackProvider;
+import org.springframework.ai.tool.method.MethodToolCallbackProvider;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class McpApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(McpApplication.class, args);
+    }
+
+    @Bean
+    public ToolCallbackProvider weatherTools(WeatherService weatherService) {
+        return  MethodToolCallbackProvider.builder().toolObjects(weatherService).build();
     }
 
 }
